8efb4bf40946f31f32c45c5ff962af6f267ef302
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-06-22 17:21:24 -0700

    Add DiceView and make it the root view for the application

---
+++ HeroDice/DiceView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title)
+    }
+}
+
+struct DiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiceView()
+    }
+}
